9f811b759959a9ccbd839e8e1c3a2564cee18c0f
Leonardo Linero <leonardoraulinero@gmail.com>
2019-07-12 23:44:20 -0500

    manejo del async storage

--- src/components/UI/AddACard.js
+++ src/components/UI/AddACard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text,StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { Card, CardItem, Button, Item, Label, Input } from 'native-base';
+import AppAsyncStorage from '../common/AppAsyncStorage';
 class AddACard extends Component {
     constructor(props) {
         super(props);
@@ -8,43 +9,57 @@ class AddACard extends Component {
             cardValue: '',
             cards: []
         }
+        this.loadCards();//<-- nuevo
     }
-    
+
+    //esto es lo nuevo
+    async loadCards() {
+        console.log('cargando las cartas')
+        let mazo = JSON.parse(await AppAsyncStorage.retrieveData("cards"))
+        this.setState({
+            cards: mazo != null ? mazo : [] 
+        })
+    }
+    //nuevo
+
     static navigationOptions = ({ navigation }) => {
         return {
             title: 'Formulario'
         }
 
     };
-    
+
 
     handleInputText(value) {
         this.setState({
             cardValue: value
         })
     }
-    
+
     handleSubmit() {
         let cardsCopy = this.state.cards.slice()
         let card = {
             value: this.state.cardValue
         }
         cardsCopy.push(card)
+        this.saveCard(cardsCopy)
+
+    }
+    async saveCard(cardsCopy){
+        await AppAsyncStorage.storeData("cards",JSON.stringify(cardsCopy))
         alert('Tarjeta Ingresada')
         this.setState({
             cardValue: '',
             cards: cardsCopy
         })
-
     }
-    //nuevo
-    goToTraining(){
+   
+    goToTraining() {
         const { navigate } = this.props.navigation;
         navigate('Training', { cards: this.state.cards })
     }
-    //nuevo
+   
     render() {
-        console.log('cards : ' + JSON.stringify(this.state.cards))//<--para testear
         return (
 
             <View style={{ padding: 10 }}>
@@ -71,7 +86,7 @@ class AddACard extends Component {
                     </CardItem>
                     <CardItem footer>
                         <Button
-                            onPress={() => this.handleSubmit()}//<------nuevo
+                            onPress={() => this.handleSubmit()}
                             block
                             light
                         >
@@ -81,16 +96,16 @@ class AddACard extends Component {
                         </Button>
                     </CardItem>
                 </Card>
-                {/* esto es nuevo */}
-                <Button block 
-                    disabled={this.state.cards.length==0}
-                    onPress={()=>this.goToTraining()}
-                 >
+                
+                <Button block
+                    disabled={this.state.cards.length == 0}
+                    onPress={() => this.goToTraining()}
+                >
                     <Text>
                         Entrenamiento
                     </Text>
                 </Button>
-                {/* esto es nuevo */}
+               
             </View>
 
 
@@ -101,9 +116,9 @@ class AddACard extends Component {
 export default AddACard;
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: 'skyblue',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        backgroundColor: 'skyblue',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-  });
+});
